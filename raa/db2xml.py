ef08671cf9417c94191ad5f85d8012c5f9bbce62
HoekR <rik.hoekstra@gmail.com>
2020-06-03 16:04:09 +0200

    updating sources (mainly raa)

--- raa/db2xml.py
+++ raa/db2xml.py
@@ -9,15 +9,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, relation, backref, scoped_session
 from sqlalchemy.orm.query import Query
 from biodes.biodes10 import BioDesDoc
-from gerbrandyutils import sh
+#from gerbrandyutils import sh
 import os
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
 OUT_DIR = os.path.join(this_dir, 'out')
 
 Base = declarative_base()
-metadata = Base.metadata 
-engine = create_engine('mysql://root@localhost/webraa')
+metadata = Base.metadata
+engine = create_engine("mysql+pymysql://rik:X0chi@localhost/raa_new")
 
 Session = sessionmaker(bind=engine)
 
@@ -60,7 +60,7 @@ class Alias(Base):
 from names import Naam
 import os
 import re
-import urllib
+import urllib.request, urllib.parse, urllib.error
 from hashlib import md5
 from lxml import etree
 from sutils import Base, sanitize_name, compress_output_files
@@ -69,57 +69,57 @@ from biodes import BioDesDoc
 
 class Main(Base):
     def write(self):
-        session=Session() 
+        session=Session()
         i = 0
         for r in session.query(Persoon).all():
             i += 1
             prepositie= ''
             intrapositie = ''
-    
+
             if r.adellijke_titel and r.adellijke_titel.naam:
                 if r.adellijke_titel.naam.lower() == 'jonkheer':
                     prepositie +=  'jonkheer '
                 else:
                     intrapositie += r.adellijke_titel.naam
-    
+
             if r.academische_titel and r.academische_titel.naam:
-                prepositie += unicode(r.academische_titel.naam, 'latin1') 
-    
+                prepositie += str(r.academische_titel.naam)
+
             if r.tussenvoegsel:
-                intrapositie += ' ' + unicode(r.tussenvoegsel, 'latin1')
-    
+                intrapositie += ' ' + str(r.tussenvoegsel)
+
             prepositie = prepositie.strip()
             intrapositie = intrapositie.strip()
             naam = Naam(
                     prepositie=prepositie,
-                    voornaam=unicode(r.voornaam or '', 'latin1') ,
+                    voornaam=str(r.voornaam or '') ,
                     intrapositie=intrapositie,
-                    geslachtsnaam=unicode(r.geslachtsnaam or '', 'latin1') ,
+                    geslachtsnaam=str(r.geslachtsnaam or '') ,
                     )
-         
-            aliassen = [Naam(unicode(alias.naam, 'latin1')) for alias in r.aliassen]
+
+            aliassen = [Naam(str(alias.naam)) for alias in r.aliassen]
             names =  [naam] + aliassen
             for n in names:
                 assert isinstance(n, Naam), names
-            print 'adding %s' % names[0]
+            print('adding %s' % names[0].volledige_naam())
             biodesdoc = BioDesDoc()
-            self.print_progress(i, names[0])
-    
+            self.print_progress(i, names[0].volledige_naam())
+
             bdate = r.geboortedatum and str(r.geboortedatum) or ''
-            if bdate.endswith('-01-01') or bdate.endswith('-12-31'): 
+            if bdate.endswith('-01-01') or bdate.endswith('-12-31'):
                 bdate = bdate[:4]
-    
+
             ddate = r.overlijdensdatum and str(r.overlijdensdatum) or ''
-            if ddate.endswith('-01-01') or ddate.endswith('-12-31'): 
+            if ddate.endswith('-01-01') or ddate.endswith('-12-31'):
                 ddate = ddate[:4]
-                
+
             try:
-                birth_place = r.geboorteplaats and unicode(r.geboorteplaats) or ''
-                death_place = r.overlijdensplaats and unicode(r.overlijdensplaats) or ''
+                birth_place = r.geboorteplaats and str(r.geboorteplaats) or ''
+                death_place = r.overlijdensplaats and str(r.overlijdensplaats) or ''
             except:
-                birth_place = r.geboorteplaats and unicode(r.geboorteplaats, 'latin1') or ''
-                death_place = r.overlijdensplaats and unicode(r.overlijdensplaats, 'latin1') or ''
-        
+                birth_place = r.geboorteplaats and str(r.geboorteplaats) or ''
+                death_place = r.overlijdensplaats and str(r.overlijdensplaats) or ''
+
             biodesdoc.from_args(
                 names = names,
                 birth_date=bdate or '',
@@ -132,7 +132,7 @@ class Main(Base):
                 url_publisher=base_url,
                 )
             self.write_file(biodesdoc, r.id, folder_name=OUT_DIR)
-    
+
 def create_files():
     for x in os.listdir(OUT_DIR):
         fn = os.path.join(OUT_DIR, x)
@@ -142,25 +142,25 @@ def create_files():
     compress_output_files(OUT_DIR)
 def upload_results():
     cmd = 'cd %s;svn ci . -m ""' % this_dir
-    print cmd
+    print(cmd)
     sh(cmd)
-    
-    
 
-def refresh_bioport_test(): 
-    x = urllib.urlopen('http://jelle:j3ll3@dev.inghist.nl/bioport/admin/source?source_id=raa&form.actions.download_biographies=1')
-    print x.read()
-    
+
+
+def refresh_bioport_test():
+    x = urllib.request.urlopen('http://jelle:j3ll3@dev.inghist.nl/bioport/admin/source?source_id=raa&form.actions.download_biographies=1')
+    print(x.read())
+
 if __name__ == '__main__':
     r = Main()
-#    
-#    print 'creating files'
+#
+    print ('creating files')
     create_files()
-    import ipdb;ipdb.set_trace() 
-    compress_output_files(OUT_DIR)
-    print 'uploading'
-    import ipdb;ipdb.set_trace() 
-    upload_results()
-    print 'refresh bioport test (will take a while...)'
-    refresh_bioport_test()
-    print 'done!'
+    #import ipdb;ipdb.set_trace()
+    #compress_output_files(OUT_DIR)
+    #print('uploading')
+    #import ipdb;ipdb.set_trace()
+    #upload_results()
+    #print('refresh bioport test (will take a while...)')
+    #refresh_bioport_test()
+    print('done!')
